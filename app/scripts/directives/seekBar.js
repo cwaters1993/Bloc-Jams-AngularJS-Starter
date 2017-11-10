953b86dceae7cc3035a0f79405e9678b2bb6e294
Christian Waters <christianwaters1993@gmail.com>
2017-11-10 16:36:59 -0500

    thumb tracker

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -31,6 +31,9 @@
                 return {width: percentString()};
             };
 
+            scope.thumbStyle = function() {
+                return {left: percentString()};
+            }
             scope.onClickSeekBar = function(event) {
                 var percent = calculatePercent(seekBar, event);
                 scope.value = percent * scope.max;
