d716e9af68c8d87061a7931d9833f5c422292aec
Christian Waters <christianwaters1993@gmail.com>
2017-11-15 22:07:36 -0500

    autoplay

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -32,6 +32,9 @@
            currentBuzzObject.bind('timeupdate', function() {
              $rootScope.$apply(function() {
                SongPlayer.currentTime = currentBuzzObject.getTime();
+               if (SongPlayer.currentTime >= SongPlayer.currentSong.duration) {
+                 SongPlayer.next();
+               };
              });
            });
 
