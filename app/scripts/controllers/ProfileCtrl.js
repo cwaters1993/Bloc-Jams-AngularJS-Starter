b59df7d2d318214839eb26cd16652970d75e0d17
Christian Waters <christianwaters1993@gmail.com>
2017-11-20 21:22:42 -0500

    temporarily edit profile info

--- app/scripts/controllers/ProfileCtrl.js
+++ app/scripts/controllers/ProfileCtrl.js
@@ -1,9 +1,11 @@
 (function() {
-    function ProfileCtrl(UserData, $location) {
+    function ProfileCtrl(Auth, UserData, $location) {
       var pc = this
       pc.id = $location.search().userid;
       pc.username = $location.search().username;
-      this.validUrl = function() {
+      pc.editingSong = false;
+      pc.editingBand = false;
+      pc.validUrl = function() {
         if(pc.id == null) {return false};
         if(pc.username == null) {return false};
         if(UserData.getUserByID(pc.id).username == pc.username) {
@@ -12,18 +14,34 @@
           return false;
         };
       };
-      this.favSong = function() {
-        return UserData.getUserByID(pc.id).favSong;
-      }
-      this.favBand = function() {
-        return UserData.getUserByID(pc.id).favBand;
-      }
-      this.profPic = function() {
-        return UserData.getUserByID(pc.id).profPic;
-      }
+      pc.favSong = (pc.validUrl() ? UserData.getUserByID(pc.id).favSong : null);
+      pc.favBand = (pc.validUrl() ? UserData.getUserByID(pc.id).favBand : null);
+      pc.profPic = (pc.validUrl() ? UserData.getUserByID(pc.id).profPic : null);
+      pc.canEdit = function() {
+        if (pc.validUrl() && (pc.username == Auth.username) && Auth.loggedIn) {
+          return true;
+        }
+        return false;
+      };
+      pc.editSong = function() {
+        if (pc.canEdit()) {
+          pc.editingSong = true;
+        }
+      };
+      pc.editBand = function() {
+        if (pc.canEdit()) {
+          pc.editingBand = true;
+        }
+      };
+      pc.saveSong = function() {
+        pc.editingSong = false;
+      };
+      pc.saveBand = function() {
+        pc.editingBand = false;
+      };
     }
 
     angular
         .module('blocJams')
-        .controller('ProfileCtrl', ['UserData', '$location', ProfileCtrl]);
+        .controller('ProfileCtrl', ['Auth', 'UserData', '$location', ProfileCtrl]);
 })();
